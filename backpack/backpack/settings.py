a6c7e33de62e389eda3bf59e9f292ea40ef6ce89
juraev-404 <153009127+juraev-404@users.noreply.github.com>
2024-02-19 19:57:12 +0500

    Update settings.py

--- backpack/backpack/settings.py
+++ backpack/backpack/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vas^1oo$i6#oh%gt!pfwclqb7v@pf)p=e=s6enx5c*i!h!nfur'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
